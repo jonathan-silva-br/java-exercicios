63f5dab6898e1bb9cea429aa395f7027398b67ba
silva_jonathan <jonathan.silva@hbsis.com.br>
2020-04-29 11:01:24 -0300

    Exercícios 9.10.1

---
+++ src/br/com/caelum/ed/conjuntos/ConjuntoEspalhamento.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.caelum.ed.conjuntos;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author jonathan.silva
+ */
+public class ConjuntoEspalhamento {
+    
+    private List<List<String>> tabela = new ArrayList<List<String>>();
+    
+    public ConjuntoEspalhamento(){
+        for(int i = 0; i < 26; i++){
+            LinkedList<String> lista = new LinkedList<String>();
+            tabela.add(lista);
+        }
+    }
+    
+}
